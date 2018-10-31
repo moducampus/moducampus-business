f3ac680dd125777344df068359466256117b479d
emiling <scopradical@gmail.com>
2018-11-01 01:46:48 +0900

    *181101 작업사항* 1.  회사 소개 Company 컴포넌트 height 100% 처리 해서 밑에 남는 공간 해결 2. 서비스 소개까지 완전한 반응형 처리
    *해결해야 할 점*
    1. Header 반응형 mobile일 때 width값
     안 맞는 것(100%?) 수정하기

--- src/components/commons/Subservice/Subservice.js
+++ src/components/commons/Subservice/Subservice.js
@@ -6,11 +6,13 @@ class Subservice extends Component {
   render() {
     return (
       <div className="Subservice">
+      <div className="service-wrapper">
         <p className="service-main-text">{this.props.name}</p>
         <a className="service-link" href="https://github.com">
           서비스 이동
         </a>
-        <p className="service-text">{this.props.info}</p>
+      </div>
+      <p className="service-text">{this.props.info}</p>
       </div>
     );
   }
